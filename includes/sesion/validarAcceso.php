03020d7352b49288c092364554adf7829ac06bc0
Fabian Castro <tafacastro9@gmail.com>
2026-05-21 16:25:26 -0500

    dd

--- includes/sesion/validarAcceso.php
+++ includes/sesion/validarAcceso.php
@@ -1,6 +1,13 @@
 <?php
 
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/db.php');
+
 
 header('Content-Type: application/json');
 
