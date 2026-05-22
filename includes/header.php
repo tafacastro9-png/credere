18ab224fc8bf419443f1cd90114fda4ff520198d
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 08:06:51 -0500

    se comenta // include "consultUserSession.php";

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include "configSession.php";
-include "consultUserSession.php";
+// include "consultUserSession.php";
 
 ?>
 <!DOCTYPE html>
