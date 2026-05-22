79983b08437d8b34ff3ef4338d2f4cdcb0e8867e
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 13:27:03 -0500

    xx

--- includes/header.php
+++ includes/header.php
@@ -1,50 +1,85 @@
-<div class="menu-toggle-btn mr-15">
+asi ? <?php
 
-    <button id="menu-toggle"
-        class="main-btn primary-btn btn-hover">
+include "configSession.php";
+include "consultUserSession.php";
 
-        <i class="lni lni-chevron-left me-2"></i>
-        Menu
+?>
 
-    </button>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+<link rel="stylesheet" href="../css/lineicons.css" />
+    <meta charset="UTF-8">
+    <title>TEST</title>
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/main.css" />
+
+</head>
+
+<body>
+
+<!-- PRELOADER -->
+<div id="preloader">
+    <div class="spinner"></div>
 </div>
 
-<div class="profile-box ml-15">
+<!-- SIDEBAR -->
+<aside class="sidebar-nav-wrapper">
 
-    <button class="dropdown-toggle bg-transparent border-0"
-        type="button"
-        id="profile"
-        data-bs-toggle="dropdown"
-        aria-expanded="false">
+    <div class="navbar-logo">
+        <h3>SIDEBAR OK</h3>
+    </div>
 
-        <div class="profile-info">
+</aside>
 
-            <div class="info">
+<!-- OVERLAY -->
+<div class="overlay"></div>
 
-                <div class="image">
-                    <img src="<?php echo $ruta_imagen; ?>" alt="" />
-                </div>
+<!-- MAIN -->
+<main class="main-wrapper">
 
-                <div>
-                    <h6 class="fw-500"><?php echo $usuario; ?></h6>
-                    <p><?php echo $user['rol']; ?></p>
+    <h1>MAIN WRAPPER OK</h1>
+    <h1>HEADER OK</h1>
+
+    <header class="header">
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-lg-5 col-md-5 col-6">
+
+                <div class="header-left d-flex align-items-center">
+
+                    <div class="menu-toggle-btn mr-15">
+
+                        <button id="menu-toggle"
+                            class="main-btn primary-btn btn-hover">
+
+                            <i class="lni lni-chevron-left me-2"></i>
+                            Menu
+
+                        </button>
+
+                    </div>
+
                 </div>
 
             </div>
 
         </div>
 
-    </button>
+    </div>
 
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profile">
+</header>
 
-        <li>
-            <a href="../views/perfilUser.php">
-                Perfil
-            </a>
-        </li>
+</main>
 
-    </ul>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/main.js"></script>
 
-</div>
+</body>
+</html>
