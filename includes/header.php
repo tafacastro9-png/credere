f42ce26ae47c0e82873058a890ef41d0508c89c0
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 12:45:34 -0500

    tt

--- includes/header.php
+++ includes/header.php
@@ -41,3 +41,35 @@ include "consultUserSession.php";
 
     <h1>MAIN WRAPPER OK</h1>
     <h1>HEADER OK</h1>
+
+    <header class="header">
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-lg-5 col-md-5 col-6">
+
+                <div class="header-left d-flex align-items-center">
+
+                    <div class="menu-toggle-btn mr-15">
+
+                        <button id="menu-toggle"
+                            class="main-btn primary-btn btn-hover">
+
+                            <i class="lni lni-chevron-left me-2"></i>
+                            Menu
+
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</header>
