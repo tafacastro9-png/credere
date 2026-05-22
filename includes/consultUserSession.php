7a948b03da0cf94827c43cf249c8da7803053327
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 11:15:39 -0500

    se de

--- includes/consultUserSession.php
+++ includes/consultUserSession.php
@@ -1,10 +1,22 @@
 <?php
+
 include "../includes/db.php";
-$sql = "SELECT  u.usuario, u.id_rol, u.imagenPerfil, r.rol FROM users u 
-LEFT JOIN roles r ON u.id_rol= r.id  WHERE usuario ='$usuario'";
+
+$usuario = $_SESSION['usuario'];
+
+$sql = "SELECT u.usuario, u.id_rol, u.imagenPerfil, r.rol 
+FROM users u 
+LEFT JOIN roles r ON u.id_rol = r.id  
+WHERE usuario ='$usuario'";
+
 $usuarios = mysqli_query($conexion, $sql);
+
 if ($usuarios->num_rows > 0) {
+
     foreach ($usuarios as $key => $user) {
+
         $ruta_imagen = $user["imagenPerfil"];
+
     }
+
 }
