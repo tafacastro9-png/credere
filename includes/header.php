a1437985d974dc09d66c724ac57f2204e6143631
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 11:54:57 -0500

    ff

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,5 @@
-<?php
-
-include "configSession.php";
-include "consultUserSession.php";
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>TEST</title>
-
-    <link rel="stylesheet" href="../css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../css/main.css" />
-
-</head>
-
 <body>
 
-<!-- PRELOADER -->
 <div id="preloader">
     <div class="spinner"></div>
 </div>
@@ -39,5 +19,21 @@ include "consultUserSession.php";
 <!-- MAIN -->
 <main class="main-wrapper">
 
-    <h1>MAIN WRAPPER OK</h1>
-    <h1>HEADER OK</h1>
+    <!-- HEADER -->
+    <header class="header">
+        <h1>HEADER OK</h1>
+    </header>
+
+    <!-- CONTENIDO -->
+    <div class="container-fluid">
+
+        <h1>MAIN WRAPPER OK</h1>
+
+    </div>
+
+</main>
+
+<script src="../js/main.js"></script>
+
+</body>
+</html>
