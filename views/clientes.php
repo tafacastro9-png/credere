3cb65665b2062d87b4008af872e3e5220375f64b
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 10:46:55 -0500

    hh

--- views/clientes.php
+++ views/clientes.php
@@ -1,10 +1,26 @@
 
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "INICIO CLIENTES<br>";
+
 include "../includes/configSession.php";
+
+echo "SESSION OK<br>";
+
 require_once "../includes/permisos.php";
+
+echo "PERMISOS OK<br>";
+
 require_once "../includes/header.php";
+
+echo "HEADER OK<br>";
+
 require_once "../includes/db.php";
 
+echo "DB OK<br>";
 
 
 error_reporting(E_ALL);
@@ -174,7 +190,7 @@ while ($fila = mysqli_fetch_assoc($result)) :
 
 </tr>             
 
-                                    <?php // include "./forms/editar_client.php"; ?>
+                                    <?php include "./forms/editar_client.php"; ?>
                                 <?php endwhile; ?>
 
                         </tbody>
