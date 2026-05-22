68eaff79a1909e986f7b5db8335ea6b2a3f72919
Fabian Castro <tafacastro9@gmail.com>
2026-05-22 12:48:04 -0500

    rr

--- includes/header.php
+++ includes/header.php
@@ -10,12 +10,12 @@ include "consultUserSession.php";
 
 <head>
 
-<link rel="stylesheet" href="../css/lineicons.css" />
     <meta charset="UTF-8">
     <title>TEST</title>
 
     <link rel="stylesheet" href="../css/bootstrap.min.css" />
     <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" href="../css/lineicons.css" />
 
 </head>
 
@@ -46,23 +46,91 @@ include "consultUserSession.php";
 
     <header class="header">
 
-    <div class="container-fluid">
+        <div class="container-fluid">
 
-        <div class="row">
+            <div class="row">
 
-            <div class="col-lg-5 col-md-5 col-6">
+                <div class="col-lg-6 col-md-6 col-6">
 
-                <div class="header-left d-flex align-items-center">
+                    <div class="header-left d-flex align-items-center">
 
-                    <div class="menu-toggle-btn mr-15">
+                        <!-- BOTON MENU -->
+                        <div class="menu-toggle-btn mr-15">
 
-                        <button id="menu-toggle"
-                            class="main-btn primary-btn btn-hover">
+                            <button id="menu-toggle"
+                                class="main-btn primary-btn btn-hover">
 
-                            <i class="lni lni-chevron-left me-2"></i>
-                            Menu
+                                <i class="lni lni-chevron-left me-2"></i>
+                                Menu
 
-                        </button>
+                            </button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-6">
+
+                    <div class="header-right d-flex justify-content-end align-items-center">
+
+                        <!-- PERFIL -->
+                        <div class="profile-box ml-15">
+
+                            <button class="dropdown-toggle bg-transparent border-0"
+                                type="button"
+                                id="profile"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false">
+
+                                <div class="profile-info">
+
+                                    <div class="info d-flex align-items-center">
+
+                                        <div class="image">
+
+                                            <img src="<?php echo $ruta_imagen; ?>"
+                                                alt=""
+                                                width="40">
+
+                                        </div>
+
+                                        <div class="ms-2">
+
+                                            <h6 class="fw-500 mb-0">
+                                                <?php echo $usuario; ?>
+                                            </h6>
+
+                                            <p class="mb-0">
+                                                <?php echo $user['rol']; ?>
+                                            </p>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </button>
+
+                            <ul class="dropdown-menu dropdown-menu-end"
+                                aria-labelledby="profile">
+
+                                <li>
+
+                                    <a class="dropdown-item"
+                                        href="../views/perfilUser.php">
+
+                                        Perfil
+
+                                    </a>
+
+                                </li>
+
+                            </ul>
+
+                        </div>
 
                     </div>
 
@@ -72,12 +140,11 @@ include "consultUserSession.php";
 
         </div>
 
-    </div>
-
-</header>
+    </header>
 
 </main>
 
+<!-- JS -->
 <script src="../js/bootstrap.bundle.min.js"></script>
 <script src="../js/main.js"></script>
 
