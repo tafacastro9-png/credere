c7e0c45394d0cd8e7dcb3209bf242c8b78ff24cd
Fabian Castro <tafacastro9@gmail.com>
2026-05-25 11:09:34 -0500

    dd

--- views/ventanaLogout.php
+++ views/ventanaLogout.php
@@ -1,22 +1,45 @@
-
-
 <!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Estas Seguro de Cerrar Sesion?</h5>
-                <button class="close btn btn-light" type="button" data-bs-dismiss="modal" aria-label="Close">
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
-                    <span class="mdi mdi-window-close"></span>
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <h5 class="modal-title" id="exampleModalLabel">
+                    Estas Seguro de Cerrar Sesion?
+                </h5>
+
+                <button type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close">
                 </button>
+
             </div>
-            <div class="modal-body">Selecciona "Logout" para cerrar sesion.</div>
+
+            <div class="modal-body">
+                Selecciona "Logout" para cerrar sesion.
+            </div>
+
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancelar</button>
-                <a class="btn btn-primary" href="../includes/sesion/cerrarSesion.php">Logout</a>
+
+                <button class="btn btn-secondary"
+                        type="button"
+                        data-bs-dismiss="modal">
+                    Cancelar
+                </button>
+
+                <a class="btn btn-primary"
+                   href="../includes/sesion/cerrarSesion.php">
+                    Logout
+                </a>
+
             </div>
+
         </div>
+
     </div>
+
 </div>
