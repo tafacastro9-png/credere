192ced77c6f1ea72f85eeca96ff0d35b81740c45
Fabian Castro <tafacastro9@gmail.com>
2026-05-25 09:14:20 -0500

    se deja el archivo tal cual

--- includes/ver_documento.php
+++ includes/ver_documento.php
@@ -1,34 +1,27 @@
 <?php
+session_start();
 
 if(!isset($_GET['archivo'])){
-    die("Archivo no especificado");
+    die("Archivo no especificado.");
 }
 
 $archivo = $_GET['archivo'];
 
+// Seguridad básica
 $archivo = str_replace(["..", "\\"], "", $archivo);
 
+// Ruta física REAL
 $ruta = __DIR__ . "/../documentos/" . $archivo;
 
-echo "<h3>DEBUG</h3>";
-
-echo "<b>Ruta:</b><br>";
-echo $ruta . "<br><br>";
-
-echo "<b>Existe:</b> ";
-echo file_exists($ruta) ? "SI" : "NO";
-echo "<br><br>";
-
-echo "<b>Readable:</b> ";
-echo is_readable($ruta) ? "SI" : "NO";
-echo "<br><br>";
-
-echo "<b>Tamaño:</b> ";
-echo file_exists($ruta) ? filesize($ruta) : "0";
-echo "<br><br>";
-
-if(file_exists($ruta)){
-    echo "<a href='/documentos/$archivo' target='_blank'>
-            ABRIR DIRECTO
-          </a>";
+// Verificar si existe
+if(!file_exists($ruta)){
+    die("Archivo no encontrado.");
 }
+
+header("Content-Type: application/pdf");
+header("Content-Disposition: inline; filename=\"" . basename($ruta) . "\"");
+header("Content-Length: " . filesize($ruta));
+
+readfile($ruta);
+exit;
+?>
