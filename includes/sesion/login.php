9a7287aa858de11489bd59b38c24eb976de04847
Fabian Castro <tafacastro9@gmail.com>
2026-05-29 14:21:16 -0500

    version estable Credere con ventana de mantenimietno

--- includes/sesion/login.php
+++ includes/sesion/login.php
@@ -1,3 +1,25 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/db.php');
+
+$queryMantenimiento = mysqli_query(
+    $conexion,
+    "SELECT valor
+     FROM parametrosinternos
+     WHERE nombre = 'mantenimiento'
+     LIMIT 1"
+);
+
+$row = mysqli_fetch_assoc($queryMantenimiento);
+
+if($row && $row['valor'] == 1){
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/mantenimiento.php';
+
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
