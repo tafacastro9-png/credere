a660cd544bac7fb206a1f975ca6301269f3ade07
Fabian Castro <tafacastro9@gmail.com>
2026-05-29 14:02:47 -0500

    ventana de publicacion

--- includes/sesion/auth.php
+++ includes/sesion/auth.php
@@ -5,6 +5,35 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/db.php');
+
+die('ENTRO AL AUTH');
+
+// =========================================
+// MODO MANTENIMIENTO
+// =========================================
+
+$queryMantenimiento = mysqli_query(
+    $conexion,
+    "SELECT valor
+     FROM parametrosinternos
+     WHERE nombre = 'mantenimiento'
+     LIMIT 1"
+);
+
+if($queryMantenimiento && mysqli_num_rows($queryMantenimiento) > 0){
+
+    $rowMantenimiento = mysqli_fetch_assoc($queryMantenimiento);
+
+    if($rowMantenimiento['valor'] == 1){
+
+        include $_SERVER['DOCUMENT_ROOT'] .
+        '/includes/mantenimiento.php';
+
+        exit();
+    }
+}
+
 // =========================================
 // EVITAR CACHE
 // =========================================
