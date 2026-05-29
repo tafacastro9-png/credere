a6e713f280349204d26ba1f12181e67fdc2f4621
Fabian Castro <tafacastro9@gmail.com>
2026-05-29 18:26:48 -0500

    filtro fechas

--- js/filterTable.js
+++ js/filterTable.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
 
     var table = $('#datatable').DataTable();
 
+    // =====================================
+    // FILTRO ESTADO
+    // =====================================
+
     $('#filtroEstado').on('change', function () {
 
         let valor = $(this).val();
-
-        console.log("Filtro cambiado:", valor);
 
         if (valor === '') {
             table.column(0).search('').draw();
@@ -16,6 +18,43 @@ $(document).ready(function () {
             table.column(0).search(valor).draw();
         }
 
+    });
+
+    // =====================================
+    // FILTRO FECHA
+    // =====================================
+
+    $.fn.dataTable.ext.search.push(
+        function(settings, data, dataIndex) {
+
+            let fechaDesde = $('#fechaDesde').val();
+            let fechaHasta = $('#fechaHasta').val();
+
+            // Columna FechaRegistro
+            let fechaTabla = data[8];
+
+            if (!fechaTabla) return true;
+
+            let fechaRegistro = fechaTabla.substring(0, 10);
+
+            if (fechaDesde && fechaRegistro < fechaDesde) {
+                return false;
+            }
+
+            if (fechaHasta && fechaRegistro > fechaHasta) {
+                return false;
+            }
+
+            return true;
+        }
+    );
+
+    // =====================================
+    // RECARGAR TABLA AL CAMBIAR FECHAS
+    // =====================================
+
+    $('#fechaDesde, #fechaHasta').on('change', function () {
+        table.draw();
     });
 
 });
