be88cf5fb388c5226a44efb7f30066b283ae071c
Fabian Castro <tafacastro9@gmail.com>
2026-05-29 14:09:14 -0500

    ws

--- includes/mantenimiento.php
+++ includes/mantenimiento.php
@@ -1,3 +1,15 @@
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/db.php');
+
+$queryMantenimiento = mysqli_query(
+    $conexion,
+    "SELECT valor
+     FROM parametrosinternos
+     WHERE nombre = 'mantenimiento'
+     LIMIT 1"
+);
+
+$row = mysqli_fetch_assoc($queryMantenimiento);
+
 if($row['valor'] == 1){
 
     die('MANTENIMIENTO ACTIVADO');
