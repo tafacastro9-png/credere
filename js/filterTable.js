e769379a5f828db78ec172abd22640c84f63f05b
Fabian Castro <tafacastro9@gmail.com>
2026-05-29 18:20:24 -0500

    hh

--- js/filterTable.js
+++ js/filterTable.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 
+    console.log("JS cargado");
+
     var table = $('#datatable').DataTable();
 
     $('#filtroEstado').on('change', function () {
 
         let valor = $(this).val();
+
+        console.log("Filtro cambiado:", valor);
 
         if (valor === '') {
             table.column(0).search('').draw();
